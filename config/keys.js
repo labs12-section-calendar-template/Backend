1d17ab18d15ad583605f17fa59d9c7cfc084afee
Zech Drinkhall <zechdrink@aol.com>
2019-04-25 15:02:51 -0700

    built out some endpoints for oauth

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// A file like this should be kept safe and secure via .gitignore
+
+module.exports = {
+    google: {
+        clientID: '2797455838-nrs2ogs7lceqtob0tb87i59q4kj9b7mp.apps.googleusercontent.com',
+        clientSecret: 'PTEOaHMN9ZBVvh4hhDpXJtsx'
+    }
+}
+
--- helpers/00-auth/auth-router.js
+++ helpers/00-auth/auth-router.js
@@ -1,54 +1,53 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs')
-const Users = require('../01-users/user-model');
-const jwt = require('jsonwebtoken');
+// const router = require('express').Router();
+// const bcrypt = require('bcryptjs')
+// const Users = require('../01-users/user-model');
+// const jwt = require('jsonwebtoken');
 
-const { generateToken } = require('./token')
+// const secret =  process.env.JWT_SECRET || 'Secrets are for the insecure'
+// const { generateToken } = require('./token')
 
-router.post('/register', async (req, res) => {
-    try {
-        let user = req.body;
-        if(user.username && user.password){
-            const hash = bcrypt.hashSync(user.password, 10);
-            user.password = hash;
-            let saved = await Users.add(user)
-            res.status(201).json(saved)
-        } else {
-            res.status(404).send("fill out the required fields")
-        }
-    } catch(error){
-        res.status(500).json(error)
-    }
-});
+// router.post('/register', async (req, res) => {
+//     try {
+//         let user = req.body;
+//         if(user.username && user.password){
+//             const hash = bcrypt.hashSync(user.password, 10);
+//             user.password = hash;
+//             let saved = await Users.add(user)
+//             res.status(201).json(saved)
+//         } else {
+//             res.status(404).send("fill out the required fields")
+//         }
+//     } catch(error){
+//         res.status(500).json(error)
+//     }
+// });
 
-router.post('/login', async (req, res) => {
+// router.post('/login', async (req, res) => {
 
-    try{
-        const {username, password } = req.body
-        user = await Users.getBy({ username });
-        if(user && bcrypt.compareSync(password, user.password)){
-            const token = generateToken(user);
-            res.status(200).json({ message: `welcome, here is your token ${user.username}`, token, user_id: user.id });
-        } else {
-            res.status(401).json({ message: 'invalid credentials' })
-        }
+//     try{
+//         const {username, password } = req.body
+//         user = await Users.getBy({ username });
+//         if(user && bcrypt.compareSync(password, user.password)){
+//             const token = generateToken(user);
+//             res.status(200).json({ message: `welcome, here is your token ${user.username}`, token, user_id: user.id });
+//         } else {
+//             res.status(401).json({ message: 'invalid credentials' })
+//         }
 
-    } catch(error){
-        res.status(500).json(error)
-    }
-})
+//     } catch(error){
+//         res.status(500).json(error)
+//     }
+// })
 
+// router.post('/checkauth', (req, res) => {
+//     const token = req.body.token;
+//     jwt.verify(token, secret, error => {
+//         if(error){
+//             res.status(501).send(false, 'uh oh')
+//         } else {
+//             res.status(200).send(true)
+//         }
+//     })
+// })
 
-//This endpoint allows for page refreshes
-router.post('/checkauth', (req, res) => {
-    const token = req.body.token;
-    jwt.verify(token, secret, error => {
-        if(error){
-            res.send(false)
-        } else {
-            res.send(true)
-        }
-    })
-})
-
-module.exports = router;
+// module.exports = router;
