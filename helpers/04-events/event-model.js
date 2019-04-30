af3e295db6440a0ac8bad9e829923011117766d0
Max Trestman <mtrestman1@gmail.com>
2019-04-30 09:52:39 -0700

    Added models for events

--- helpers/04-events/event-model.js
+++ helpers/04-events/event-model.js
@@ -1,9 +1,31 @@
 const db = require('../../database/dbConfig');
 
 module.exports = {
-    find
+    find,
+    findById,
+    add,
+    remove,
+    getBy
 }
 
 function find(){
     return db('events');
+}
+
+function findById(id){
+    return db('events').where({ id }).first()
+}
+
+async function add(event){
+    const [id] = await db('events').insert(event);
+
+    return db('events').where({ id }).first()
+}
+
+function remove(id){
+    return db('events').where({ id }).del()
+}
+
+function getBy(select){
+    return db('events').where(select).first();
 }
