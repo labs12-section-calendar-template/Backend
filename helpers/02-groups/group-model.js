0cdb79eb0089dae774bfd5c772553cac25365625
ills5771 <ilyayelizarov1@gmail.com>
2019-04-30 11:15:13 -0700

    pushing latest

--- helpers/02-groups/group-model.js
+++ helpers/02-groups/group-model.js
@@ -1,7 +1,6 @@
 const db = require("../../database/dbConfig");
 
 module.exports = {
-<<<<<<< HEAD
   find,
   findById,
   add,
@@ -10,42 +9,13 @@ module.exports = {
   getGroupTemplates,
   update
 };
-=======
-    find,
-    findById,
-    add,
-    update,
-    getBy,
-    remove,
-    getGroupTemplates
+
+function getBy(select) {
+  return db("groups")
+    .where(select)
+    .first();
 }
 
-function find(){
-    return db('groups');
-}
-
-function findById(id){
-    return db('groups').where({ id }).first()
-}
-
-async function add(group){
-    const [id] = await db('groups').insert(group);
-
-    return db('groups').where({ id }).first()
-}
-
-function update(id, changes){
-    return db('groups')
-        .where({ id })
-        .update(changes);
-}
->>>>>>> a8022f9355ea83357d99c0edc1a78c55a1e68554
-
-function getBy(select){
-    return db('groups').where(select).first();
-}
-
-<<<<<<< HEAD
 function findById(id) {
   return db("groups")
     .where({ id })
@@ -84,17 +54,3 @@ function update(id, updates) {
     .where({ id })
     .update(updates);
 }
-=======
-
-function remove(id){
-    return db('groups').where({ id }).del()
-}
-
-function getGroupTemplates(groupID){
-    return db('templates')
-        .join('groups', 'groups.id', 'templates.group_id')
-        .select('templates.*' )
-        .where('templates.group_id', groupID)
-}
-
->>>>>>> a8022f9355ea83357d99c0edc1a78c55a1e68554
