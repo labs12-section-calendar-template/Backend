9e4b4d58f351a8bc383c1106d736b52e2c2b6d7e
t-tullis <t.tullis34@gmail.com>
2019-04-30 11:10:19 -0700

    Added endpoints to create a template, get all templates, and get a template by id

--- helpers/03-templates/template-router.js
+++ helpers/03-templates/template-router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Templates = require("./template-model");
 
+//Gets all templates
 router.get("/", async (req, res) => {
   try {
     const templates = await Templates.find();
@@ -13,5 +14,38 @@ router.get("/", async (req, res) => {
     res.status(500).send(error, "server error");
   }
 });
+
+//Gets template by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const template = await Templates.findById(req.params.id);
+
+    if (template) {
+      res.status(200).json(template);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The group with that ID does not exist" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "server error" });
+  }
+});
+
+//Adds a template
+router.post('/', async (req, res) => {
+
+  try{
+      const template = await Templates.add(req.body)
+      if(template){
+          res.status(200).json(template)
+      } else{
+          res.status(404).send('the template could not be added')
+      }
+  } catch(error){
+      res.status(500).send(error, 'server error')
+  }
+})
+
 
 module.exports = router;
