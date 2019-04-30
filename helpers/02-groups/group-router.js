5d074d8da99bc321214d0ffef12dfa1a40a412e0
ills5771 <ilyayelizarov1@gmail.com>
2019-04-30 10:53:32 -0700

    adds put route to update group information

--- helpers/02-groups/group-router.js
+++ helpers/02-groups/group-router.js
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const group = await Groups.getById(req.params.id);
+    const group = await Groups.findById(req.params.id);
 
     if (group) {
       res.status(200).json(group);
@@ -44,6 +44,22 @@ router.post("/", (req, res) => {
         console.error(error);
         res.status(500).json({ error: "server error" });
       });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const group = await Groups.update(req.params.id, req.body);
+
+    if (group) {
+      res.status(200).json({ message: "The group was updated" });
+    } else {
+      res.status(404).json({ message: "The group could not be found" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating group"
+    });
   }
 });
 
