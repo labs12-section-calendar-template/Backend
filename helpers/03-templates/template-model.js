cb4f130d092bf1cf0d0f12d999b70bc1d1142c11
t-tullis <t.tullis34@gmail.com>
2019-04-30 09:41:59 -0700

    Added Find, FindBy, and Add funtions to template-model

--- helpers/03-templates/template-model.js
+++ helpers/03-templates/template-model.js
@@ -1,9 +1,21 @@
 const db = require('../../database/dbConfig');
 
 module.exports = {
-    find
+    find,
+    findById,
+    add
 }
 
 function find(){
     return db('templates');
+}
+
+function findById(id){
+    return db('templates').where({ id }).first()
+}
+
+async function add(template){
+    const [id] = await db('templates').insert(template);
+
+    return db('templates').where({ id }).first()
 }
