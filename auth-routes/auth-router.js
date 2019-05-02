fab6d3ba3445763eb54ed7e51c0d8552fbb98ae1
Zech Drinkhall <zechdrink@aol.com>
2019-05-02 11:22:40 -0700

    remove gitignore files

--- auth-routes/auth-router.js
+++ auth-routes/auth-router.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
-const key = require("../config/keys");
 require('dotenv').config();
-const localhost_url = 'http://localhost:3000'
+
 
 router.get("/login", (req, res) => {
   res.render("login", { user: req.user });
@@ -19,11 +18,7 @@ router.get("/google", passport.authenticate("google", {
 );
 
 router.get('/google/redirect', passport.authenticate('google'), (req, res) =>{
-    if(process.env.NODE_ENV === 'production'){
-      res.redirect('https://calendr.netlify.com')
-    } else {
-      res.redirect(`${localhost_url}`)
-    }
+  res.redirect(process.env.FRONT_END_URL)
 })
 
 
