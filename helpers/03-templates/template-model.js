e6daa85bcc173b0a4d38cf753b81c7b744ec9cee
ills5771 <ilyayelizarov1@gmail.com>
2019-05-09 17:05:51 -0700

    changes made to  addEventsToTemplates

--- helpers/03-templates/template-model.js
+++ helpers/03-templates/template-model.js
@@ -1,57 +1,71 @@
 const db = require("../../database/dbConfig");
 
 module.exports = {
-    find,
-    findById,
-    add,
-    getById,
-    remove,
-    update,
-    getTemplateEvents,
-    addEventsToTemplates
+  find,
+  findById,
+  add,
+  getById,
+  remove,
+  update,
+  getTemplateEvents,
+  addEventsToTemplates
+};
+
+function find() {
+  return db("templates");
 }
 
-function find(){
-    return db('templates');
+function findById(id) {
+  return db("templates")
+    .where({ id })
+    .first();
 }
 
-function findById(id){
-    return db('templates').where({ id }).first()
+async function add(template) {
+  const [id] = await db("templates").insert(template, "*");
+
+  return db("templates")
+    .where({ id })
+    .first();
 }
 
-async function add(template){
-    const [id] = await db('templates').insert(template, "*");
-
-    return db('templates').where({ id }).first()
+function getById(select) {
+  return db("templates")
+    .where(select)
+    .first();
 }
 
-function getById(select){
-    return db('templates').where(select).first();
+function remove(id) {
+  return db("templates")
+    .where({ id })
+    .del();
 }
 
-
-function remove(id){
-    return db('templates').where({ id }).del()
+function update(id, changes) {
+  return db("templates")
+    .where({ id })
+    .update(changes, "*");
 }
 
-function update(id, changes){
-    return db('templates')
-        .where({ id })
-        .update(changes, "*");
+function getTemplateEvents(templateID) {
+  return db("events")
+    .join("templates", "templates.id", "events.template_id")
+    .select("events.*")
+    .where("events.template_id", templateID);
 }
 
-function getTemplateEvents(templateID){
-    return db('events')
-        .join('templates', 'templates.id', 'events.template_id')
-        .select('events.*' )
-        .where('events.template_id', templateID)
-}
-
-function addEventsToTemplates(event){
-    return db('events')
-    .insert({
-        date: event.date,
-        time: event.time,
-        template_id: event.template_id
-    })
+async function addEventsToTemplates(event) {
+  const [id] = await db("events").insert(
+    {
+      title: event.title,
+      description: event.description,
+      date: event.date,
+      time: event.time,
+      template_id: event.template_id
+    },
+    "id"
+  );
+  return db("events")
+    .where({ id })
+    .first();
 }
